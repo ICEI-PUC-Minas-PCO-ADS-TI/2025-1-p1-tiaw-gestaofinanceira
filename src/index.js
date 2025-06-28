c65f12c34a87bbc1ba6629e4377f316ca0ee3ae2
Samuel Aguiar Carvalho Gusmão <acgsamuel92@gmail.com>
2025-06-28 12:41:18 -0300

    chore: setup inicial

--- src/index.js
+++ src/index.js
@@ -16,13 +16,13 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./db/db.json')
-  
+
 // Para permitir que os dados sejam alterados, altere a linha abaixo
 // colocando o atributo readOnly como false.
-const middlewares = jsonServer.defaults({ noCors: true })
+const middlewares = jsonServer.defaults()
+
 server.use(middlewares)
 server.use(router)
-
 server.listen(3000, () => {
-  console.log(`JSON Server is running em http://localhost:3000`)
+  console.log('JSON Server está em execução!')
 })
